603fd2f27794c5a78a1a830ffe22d21f05af9d56
walty13 <walterbeles@gmail.com>
2023-11-09 18:02:10 +0100

    added lamp mil and events

---
+++ Projet/equipments/Lamp/mil/events/SetLowLamp.java
@@ -0,0 +1,136 @@
+package equipments.Lamp.mil.events;
+
+// Copyright Jacques Malenfant, Sorbonne Universite.
+// Jacques.Malenfant@lip6.fr
+//
+// This software is a computer program whose purpose is to implement a mock-up
+// of household energy management system.
+//
+// This software is governed by the CeCILL-C license under French law and
+// abiding by the rules of distribution of free software.  You can use,
+// modify and/ or redistribute the software under the terms of the
+// CeCILL-C license as circulated by CEA, CNRS and INRIA at the following
+// URL "http://www.cecill.info".
+//
+// As a counterpart to the access to the source code and  rights to copy,
+// modify and redistribute granted by the license, users are provided only
+// with a limited warranty  and the software's author,  the holder of the
+// economic rights,  and the successive licensors  have only  limited
+// liability. 
+//
+// In this respect, the user's attention is drawn to the risks associated
+// with loading,  using,  modifying and/or developing or reproducing the
+// software by the user in light of its specific status of free software,
+// that may mean  that it is complicated to manipulate,  and  that  also
+// therefore means  that it is reserved for developers  and  experienced
+// professionals having in-depth computer knowledge. Users are therefore
+// encouraged to load and test the software's suitability as regards their
+// requirements in conditions enabling the security of their systems and/or 
+// data to be ensured and,  more generally, to use and operate it in the 
+// same conditions as regards security. 
+//
+// The fact that you are presently reading this means that you have had
+// knowledge of the CeCILL-C license and that you accept its terms.
+
+import fr.sorbonne_u.devs_simulation.models.events.EventI;
+import fr.sorbonne_u.devs_simulation.models.interfaces.AtomicModelI;
+import fr.sorbonne_u.devs_simulation.models.time.Time;
+import equipments.Lamp.mil.LampElectricityModel;
+
+// -----------------------------------------------------------------------------
+/**
+ * The class <code>SetLowLamp</code> defines the simulation event of the
+ * lamp being set to medium or low mode.
+ *
+ * <p><strong>Description</strong></p>
+ * 
+ * <p><strong>Invariant</strong></p>
+ * 
+ * <pre>
+ * invariant	{@code true}
+ * </pre>
+ * 
+ * <p>Created on : 2021-09-20</p>
+ * 
+ * @author	<a href="mailto:Jacques.Malenfant@lip6.fr">Jacques Malenfant</a>
+ */
+public class			SetLowLamp
+extends		AbstractLampEvent
+{
+	// -------------------------------------------------------------------------
+	// Constants and variables
+	// -------------------------------------------------------------------------
+
+	private static final long serialVersionUID = 1L;
+
+	// -------------------------------------------------------------------------
+	// Constructors
+	// -------------------------------------------------------------------------
+
+	/**
+	 * create a <code>SetLowLamp</code> event.
+	 * 
+	 * <p><strong>Contract</strong></p>
+	 * 
+	 * <pre>
+	 * pre	{@code true}	// no more precondition.
+	 * post	{@code true}	// no more postcondition.
+	 * </pre>
+	 *
+	 * @param timeOfOccurrence	time of occurrence of the event.
+	 */
+	public				SetLowLamp(Time timeOfOccurrence)
+	{
+		super(timeOfOccurrence, null);
+	}
+
+	// -------------------------------------------------------------------------
+	// Methods
+	// -------------------------------------------------------------------------
+
+	/**
+	 * @see fr.sorbonne_u.devs_simulation.es.events.ES_Event#hasPriorityOver(fr.sorbonne_u.devs_simulation.models.events.EventI)
+	 */
+	@Override
+	public boolean			hasPriorityOver(EventI e)
+	{
+		// if many lamp events occur at the same time, the
+		// SetLOWLamp one will be executed first except for
+		// SwitchOnLamp ones.
+		if (e instanceof SwitchOnLamp) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	/**
+	 * @see fr.sorbonne_u.devs_simulation.models.events.Event#executeOn(fr.sorbonne_u.devs_simulation.models.interfaces.AtomicModelI)
+	 */
+	@Override
+	public void				executeOn(AtomicModelI model)
+	{
+		assert	model instanceof LampElectricityModel;
+
+		LampElectricityModel m = (LampElectricityModel)model;
+		// a SetMedium event can only be executed when the state of the lamp
+		// model is in the state HIGH
+		if (m.getState() == LampElectricityModel.State.HIGH) {
+			// then put it in the state MEDIUM
+			m.setState(LampElectricityModel.State.MEDIUM);
+			// trigger an internal transition by toggling the electricity
+			// consumption changed boolean to true
+			m.toggleConsumptionHasChanged();
+		}
+		// a SetLow event can only be executed when the state of the lamp
+		// model is in the state MEDIUM
+		if (m.getState() == LampElectricityModel.State.MEDIUM) {
+			// then put it in the state LOW
+			m.setState(LampElectricityModel.State.LOW);
+			// trigger an internal transition by toggling the electricity
+			// consumption changed boolean to true
+			m.toggleConsumptionHasChanged();
+		}
+	}
+}
+// -----------------------------------------------------------------------------
